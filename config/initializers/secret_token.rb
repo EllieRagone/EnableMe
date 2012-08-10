47b7ff25f3b7a038fbd18ce5bdbe7aa37a7ec3bb
DavidRagone <user@server.com>
2012-08-09 20:46:33 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnableMe::Application.config.secret_token = '9cb91de43439d6f322767effe3dce7156bb3117ef878b28bf85012f5421bf2df996c277d7a485893cf0fb90e4b9f6c9a35fd9d4b10902b309d02170a97a479fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EnableMe::Application.config.session_store :cookie_store, key: '_EnableMe_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EnableMe::Application.config.session_store :active_record_store
